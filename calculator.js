4c57447ad53606413618780f5d8eaf4f4452a65f
abubakar <abubakarjuttabubakar553@gmail.com>
2023-08-05 08:52:42 -0700

    my animated calculater

---
+++ calculator.js
@@ -0,0 +1,31 @@
+let input = document.getElementById('inputdata');
+let buttons = document.querySelectorAll('button');
+let string="";
+let aray = Array.from(buttons);
+aray.forEach(button=>
+    {
+        button.addEventListener('click',(e)=>
+        {
+            if(e.target.innerHTML=='=')
+            {
+                string=eval(string);
+                input.value=string;
+
+            }
+            else if(e.target.innerHTML == 'AC'){
+                string ="";
+                input.value=string;
+            }
+            else if(e.target.innerHTML == 'DEL'){
+                string = string.substring(0,string.length-1);
+                input.value = string;
+                
+            }
+            else
+            {
+                string += e.target.innerHTML;
+                input.value=string;
+            }
+             
+        })
+    })
